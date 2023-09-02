7d03a596ad15cfade6daba7d0a5319b82258a902
Viktoriia <pecourses>
2023-09-02 12:13:39 +0300

    Add connection with store to offers page

--- client/src/pages/OffersPage/OffersPage.jsx
+++ client/src/pages/OffersPage/OffersPage.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import { connect } from 'react-redux';
+import { getOffers } from '../../store/slices/offersSlice';
+import Spinner from '../../components/Spinner/Spinner';
 
-function OffersPage () {
+function OffersPage ({ isFetching, error, offers, getOffers }) {
+  useEffect(() => {
+    getOffers();
+  }, []);
+  // TODO Handle Error
   return (
     <>
       <Header />
-      <div>OffersPage</div>
+      <div>
+        {isFetching && <Spinner />}
+        {error && <div>ERROR!!!</div>}
+        {!isFetching && !error && <div>{JSON.stringify(offers)}</div>}
+      </div>
       <Footer />
     </>
   );
 }
 
-export default OffersPage;
+const mapStateToProps = state => state.offersList;
+
+const mapDispathToProps = dispatch => ({
+  getOffers: () => dispatch(getOffers()),
+});
+
+export default connect(mapStateToProps, mapDispathToProps)(OffersPage);
