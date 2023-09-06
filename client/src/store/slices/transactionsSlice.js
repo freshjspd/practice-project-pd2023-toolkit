391c8df5167048f606193d8f008c8c6c770ed7d9
Viktoriia <pecourses>
2023-09-06 03:56:48 +0300

    Refactor transaction slice

--- client/src/store/slices/transactionsSlice.js
+++ client/src/store/slices/transactionsSlice.js
@@ -1,6 +1,11 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import * as restController from './../../api/rest/restController';
-import { pendingReducer, rejectedReducer } from '../../utils/store';
+import {
+  createExtraReducers,
+  decorateAsyncThunk,
+  pendingReducer,
+  rejectedReducer,
+} from '../../utils/store';
 
 const TRANSACTIONS_SLICE_NAME = 'transactions';
 
@@ -10,29 +15,23 @@ const initialState = {
   transactions: [],
 };
 
-export const getTransactions = createAsyncThunk(
-  `${TRANSACTIONS_SLICE_NAME}/get`,
-  async (payload, { rejectWithValue }) => {
-    try {
-      const { data } = await restController.getTransactions();
-      return data;
-    } catch (err) {
-      return rejectWithValue({
-        data: err?.response?.data ?? 'Gateway Timeout',
-        status: err?.response?.status ?? 504,
-      });
-    }
-  }
-);
+export const getTransactions = decorateAsyncThunk({
+  key: `${TRANSACTIONS_SLICE_NAME}/get`,
+  thunk: async () => {
+    const { data } = await restController.getTransactions();
+    return data;
+  },
+});
 
-const extraReducers = (builder) => {
-  builder.addCase(getTransactions.pending, pendingReducer);
-  builder.addCase(getTransactions.fulfilled, (state, { payload }) => {
+const extraReducers = createExtraReducers({
+  thunk: getTransactions,
+  pendingReducer,
+  fulfilledReducer: (state, { payload }) => {
     state.transactions = [...payload];
     state.isFetching = false;
-  });
-  builder.addCase(getTransactions.rejected, rejectedReducer);
-};
+  },
+  rejectedReducer,
+});
 
 const transactionsSlice = createSlice({
   name: TRANSACTIONS_SLICE_NAME,
